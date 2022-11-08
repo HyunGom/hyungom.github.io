4dd502fa9d6a0d9ffd3fc60c3a102b2f8411ed8a
Hyunook Kwon <hyunook23@gmail.com>
2022-11-07 19:43:51 -0500

    portfolio section with Swiper functionality

--- file/js/main.js
+++ file/js/main.js
@@ -97,6 +97,21 @@ servicesModalClose.forEach((modalClose) => {
 })
 
 
+// Swiper
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
